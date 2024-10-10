83303a47954b35855de712cf7a1d9e00566ec9e5
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-10-10 19:39:53 +0100

    Adding Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,8 +8,7 @@ sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 def canUnlockAll(boxes):
     """
-    You have n number of locked boxes in front of you. Each box is numbered
-    sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+    The function
     """
 
     keys = boxes[0]
@@ -20,7 +19,7 @@ def canUnlockAll(boxes):
         key = keys.pop(0)
         if key >= number_of_boxes:
             continue
-        if not key in opened:
+        if key not in opened:
             opened.append(key)
             for key in boxes[key]:
                 keys.append(key)
