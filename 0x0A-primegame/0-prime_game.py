8db06bd611be37441325c27dabb007fcdc432fb0
AhmedElKhawlani <elkhawlani.python@gmail.com>
2025-01-18 13:29:20 +0100

    Interview Late commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Prime Game Solution
+This script defines a solution to determine the winner of the Prime Game.
+"""
+
+
+def generate_primes(n):
+    """
+    Generate a list of prime numbers from 1 to n (inclusive).
+
+    Args:
+        n (int): The upper limit of the range.
+
+    Returns:
+        list: A list of prime numbers within the range.
+    """
+    primes_list = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            primes_list.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return primes_list
+
+
+def is_winner(rounds, nums):
+    """
+    Determine the winner of the Prime Game.
+
+    Args:
+        rounds (int): The number of rounds to play.
+        nums (list): List of upper limits for each round.
+
+    Returns:
+        str: The name of the winner ("Maria" or "Ben"),
+        or None if there's no winner.
+    """
+    if not rounds or not nums or rounds == 0 or not nums:
+        return None
+
+    maria_score = 0
+    ben_score = 0
+
+    for i in range(rounds):
+        primes_list = generate_primes(nums[i])
+        if len(primes_list) % 2 == 0:
+            ben_score += 1
+        else:
+            maria_score += 1
+
+    if maria_score > ben_score:
+        return "Maria"
+    elif ben_score > maria_score:
+        return "Ben"
+    return None
